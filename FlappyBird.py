522569f11fc6149acb98c308e9104c71e4947ddc
kinga012101 <262219@student.pwr.edu.pl>
2021-06-05 23:13:07 +0200

    Update FlappyBird.py

--- FlappyBird.py
+++ FlappyBird.py
@@ -1,4 +1,4 @@
-import pygame, sys
+import pygame, sys, random 
 from datetime import datetime
 
 
@@ -15,13 +15,34 @@ def rotation(bird):
     new_bird = pygame.transform.rotozoom(bird,-bird_movement*2, 1)
     return new_bird
 
+def cretation():
+    position= random.choice(pipe_height)
+    bottom_pipe = pip_surface.get_rect(midtop = (300,position))
+    top_pipe = pip_surface.get_rect(midbottom = (300, position - 150))
+    return bottom_pipe, top_pipe
+
+def showing_pipes(pipes):
+    for i in pipes:
+        if i.bottom >= 512:
+            screen.blit(pip_surface,i)
+        else:
+            flip = pygame.transform.flip(pip_surface, False, True)
+            screen.blit(flip,i)
+
+def moving_pipes(pipes):
+    for i in pipes: 
+        i.centerx -= 2
+    current_pipes = [i for i in pipes if i.right >= -50]
+    return current_pipes
+
+
 
 
 pygame.init()
 screen = pygame.display.set_mode((288, 512))
 clock = pygame.time.Clock()
 
-gravity = 0.15
+gravity = 0.1
 bird_movement = 0
 game_on = True
 
@@ -36,7 +57,7 @@ else:
 floor = pygame.image.load('assets/base.png').convert()
 floor_position = 0
 
-#bird
+# bird
 bird_downflap = pygame.image.load('assets/yellowbird-downflap.png').convert_alpha()
 bird_midflap = pygame.image.load('assets/yellowbird-midflap.png').convert_alpha()
 bird_upflap = pygame.image.load('assets/yellowbird-upflap.png').convert_alpha()
@@ -45,12 +66,26 @@ bird_index = 0
 bird_surface = bird_moves[bird_index]
 bird_rectangle = bird_surface.get_rect(center = (50,256))
 
+# pipes 
+if date.hour < 20 :
+    pip_surface = pygame.image.load('assets/pipe-green.png').convert()
+else:
+    pip_surface = pygame.image.load('assets/pipe-red.png').convert()
+pipes = []
+SPWAN = pygame.USEREVENT
+pygame.time.set_timer(SPWAN, 1000)
+pipe_height = [200,300,400]
+
+
 
 while True:
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             pygame.quit()
             sys.exit()
+
+        if event.type == SPWAN:
+            pipes.extend(cretation())
 
     screen.blit(background, (0, 0))
 
@@ -61,6 +96,11 @@ while True:
         bird_rectangle.centery += bird_movement
         screen.blit(rotated_bird, bird_rectangle)
 
+        # Pipes 
+        pipes = moving_pipes(pipes)
+        showing_pipes(pipes)
+
+
     # Floor
     floor_position -= 1
     moving_floor()
@@ -69,4 +109,3 @@ while True:
 
     pygame.display.update()
     clock.tick(120)
-    
